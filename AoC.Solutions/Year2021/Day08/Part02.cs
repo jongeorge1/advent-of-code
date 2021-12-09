68b7319fc018eff126ff5c3a02f8f0a8809fbace
Jonathan George <jon@jonathangeorge.co.uk>
2021-12-09 09:30:11 +0000

    2021 day 8

--- AoC.Solutions/Year2021/Day08/Part02.cs
+++ AoC.Solutions/Year2021/Day08/Part02.cs
@@ -7,33 +7,8 @@
 
     public class Part02 : ISolution
     {
-        private static readonly char[][] Numbers = new[]
-        {
-            new char[0],
-            new[] { 'c', 'f' },
-            new[] { 'a', 'c', 'd', 'e', 'g' },
-            new[] { 'a', 'c', 'd', 'f', 'g' },
-            new[] { 'b', 'c', 'd', 'f' },
-            new[] { 'a', 'b', 'd', 'f', 'g' },
-            new[] { 'a', 'b', 'd', 'e', 'f', 'g' },
-            new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' },
-            new[] { 'a', 'b', 'c', 'd', 'f', 'g' },
-        };
-
         public string Solve(string input)
         {
-            // Segment counts:
-            // 1 - 2
-            // 2 - 5
-            // 3 - 5
-            // 4 - 4
-            // 5 - 5
-            // 6 - 6
-            // 7 - 3
-            // 8 - 7
-            // 9 - 6
-            // This means the numbers that use unique segment counts are:
-            // 1, 4, 7 and 8
             var inputLines = input.Split(Environment.NewLine).ToList();
 
             (string[] Inputs, string[] Outputs)[] digits = inputLines.Select(x => x.Split('|'))
@@ -48,38 +23,49 @@
 
         private static int Decode((string[] Inputs, string[] Outputs) display)
         {
-            var allCodes = new List<string>(display.Inputs.Union(display.Outputs));
+            // The inputs contain an example of every digit. We use this to work out what's what:
+            // - The one with 2 characters is the 1
+            // - The one with 3 characters is the 7
+            // - The one with 4 characters is the 4
+            // - The one with 7 characters is the 8
+            // - If we then look at the ones with 5 characters (2, 3, 5) and find the one that contains the 2 signals for the number 1, that will be the three.
+            // - Recap: we now know 1, 3, 4, 7 and 8.
+            // - If we then look at the ones with 6 (0, 6, 9) characters and find the one that doesn't contain the two signals for the number 1, that will be the 6.
+            // - Recap: we now know 1, 3, 4, 6, 7 and 8.
+            // - We can look at the difference between 6 and 8 to tell us what is the signal for position c.
+            // - Since we know c, we can look at the code for 1 to find out f.
+            // - We now look at the two remaining digits which both have 5 characters, which are 2 and 5. The one that contains the signal for c, but not f is the two, the other is the 5.
+            // - Recap: we now know 1, 2, 3, 4, 5, 6, 7 and 8.
+            // - Finally we look at the two remaining with 6 digits, 0 and 9. The one that contains all the signals of 3 is 9. The other is 0.
+            // We can then match the outputs against the inputs to determine the code. There's not guarantee that the order of signals in the outputs
+            // will match the orders in the inputs, so we sort them before getting started.
+            Dictionary<int, string> digits = new ();
+            Dictionary<char, char> signals = new ();
 
-            // We need to identity the candidates for each segment. Then we'll repeatedly
-            // iterate until we only have one remaining option for each
-            var candidateMappings = new Dictionary<char, List<char>>
-            {
-                { 'a', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-                { 'b', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-                { 'c', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-                { 'd', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-                { 'e', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-                { 'f', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-                { 'g', new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-            };
+            string[] inputs = display.Inputs.Select(x => string.Concat(x.OrderBy(y => y))).ToArray();
+            string[] outputs = display.Outputs.Select(x => string.Concat(x.OrderBy(y => y))).ToArray();
 
-            ReduceCandidates(candidateMappings, allCodes.FirstOrDefault(x => x.Length == 2), Numbers[2]);
-            ReduceCandidates(candidateMappings, allCodes.FirstOrDefault(x => x.Length == 4), Numbers[4]);
-            ReduceCandidates(candidateMappings, allCodes.FirstOrDefault(x => x.Length == 3), Numbers[7]);
-            ReduceCandidates(candidateMappings, allCodes.FirstOrDefault(x => x.Length == 7), Numbers[8]);
+            digits[1] = inputs.First(x => x.Length == 2);
+            digits[7] = inputs.First(x => x.Length == 3);
+            digits[4] = inputs.First(x => x.Length == 4);
+            digits[8] = inputs.First(x => x.Length == 7);
+            digits[3] = inputs.First(x => x.Length == 5 && x.Contains(digits[1][0]) && x.Contains(digits[1][1]));
+            digits[6] = inputs.First(x => x.Length == 6 && (x.Contains(digits[1][0]) ^ x.Contains(digits[1][1])));
 
-            return 0;
-        }
+            char signalC = digits[8].First(x => !digits[6].Contains(x));
 
-        private static void ReduceCandidates(Dictionary<char, List<char>> candidateMappings, string? inputs, char[] potentialOutputs)
-        {
-            if (inputs is not null)
-            {
-                foreach (char current in inputs)
-                {
-                    candidateMappings[current] = candidateMappings[current].Intersect(potentialOutputs).ToList();
-                }
-            }
+            digits[2] = inputs.First(x => x.Length == 5 && x != digits[3] && x.Contains(signalC));
+            digits[5] = inputs.First(x => x.Length == 5 && x != digits[3] && x != digits[2]);
+
+            digits[9] = inputs.First(x => x.Length == 6 && x != digits[6] && digits[3].All(d => x.Contains(d)));
+            digits[0] = inputs.First(x => x.Length == 6 && x != digits[6] && x != digits[9]);
+
+            var digitCodes = digits.ToDictionary(x => string.Concat(x.Value.OrderBy(x => x)), x => x.Key);
+
+            return (digitCodes[string.Concat(outputs[0].OrderBy(x => x))] * 1000) +
+                (digitCodes[string.Concat(outputs[1].OrderBy(x => x))] * 100) +
+                (digitCodes[string.Concat(outputs[2].OrderBy(x => x))] * 10) +
+                digitCodes[string.Concat(outputs[3].OrderBy(x => x))];
         }
     }
 }
