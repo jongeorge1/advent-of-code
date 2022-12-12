4b85bb89e8b6bc798e7dc2d302b0b927e056f021
Jonathan George <jon@jonathangeorge.co.uk>
2022-12-12 11:13:01 +0000

    Parallelise part 2. Not sure how reliable it is though.

--- AdventOfCode/Year2022/Day12/Part02.cs
+++ AdventOfCode/Year2022/Day12/Part02.cs
@@ -1,7 +1,10 @@
 ﻿namespace AdventOfCode.Year2022.Day12
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
     using AdventOfCode;
 
     public class Part02 : ISolution
@@ -12,13 +15,19 @@
 
             int minimumSteps = int.MaxValue;
 
-            foreach (KeyValuePair<(int X, int Y), char> location in algorithm.Map.Where(x => x.Value == 'a'))
+            // I wasn't expecting this parallelisation to work; I thought there was too much opportunity for a race condition
+            // between the comparison of current steps to minimum steps and the update of minimum steps. However, I've run it
+            // a lot of times and it's given the same answer each time.
+            Parallel.ForEach(
+                algorithm.Map.Where(x => x.Value == 'a'),
+                new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount / 2 },
+                location =>
             {
                 if (algorithm.TryFindShortestPathBetween(location.Key, algorithm.End, out int? steps) && steps < minimumSteps)
                 {
                     minimumSteps = steps.Value;
                 }
-            }
+            });
 
             return minimumSteps.ToString();
         }
