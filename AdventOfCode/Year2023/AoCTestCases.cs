7eb91f6c650414f6a3798786830b94e1925467df
Jonathan George <github@jonathangeorge.co.uk>
2023-12-04 11:49:39 +0000

    Set up day 2

--- AdventOfCode/Year2023/AoCTestCases.cs
+++ AdventOfCode/Year2023/AoCTestCases.cs
@@ -10,6 +10,7 @@ namespace AdventOfCode.Year2023
         [TestCase(1, 2, "two1", "21")]
         [TestCase(1, 2, "1two", "12")]
         [TestCase(1, 2, "nine", "99")]
+        [TestCase(2, 1, "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\r\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\r\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\r\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\r\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", "8")]
         public void Tests(int day, int part, string input, string expectedResult)
         {
             ISolution solution = SolutionFactory.GetSolution(2023, day, part);
--- AdventOfCode/Year2023/Day02/Part01.cs
+++ AdventOfCode/Year2023/Day02/Part01.cs
@@ -1,11 +1,17 @@
 ﻿namespace AdventOfCode.Year2023.Day02
 {
+    using System;
     using AdventOfCode;
+    using AdventOfCode.Helpers;
 
     public class Part01 : ISolution
     {
         public string Solve(string input)
         {
+            foreach (StringExtensions.StringSplitEntry entry in input.OptimizedSplit(Environment.NewLine.AsSpan()))
+            {
+            }
+
             return string.Empty;
         }
     }
