5cfe401ff280287e417f845e5a7c1b133aa14c52
Jonathan George <jon@jonathangeorge.co.uk>
2024-02-17 15:56:44 +0100

    Day 7

--- AdventOfCode/Year2023/AoCTestCases.cs
+++ AdventOfCode/Year2023/AoCTestCases.cs
@@ -21,6 +21,8 @@ namespace AdventOfCode.Year2023
         [TestCase(5, 2, "seeds: 79 14 55 13\r\n\r\nseed-to-soil map:\r\n50 98 2\r\n52 50 48\r\n\r\nsoil-to-fertilizer map:\r\n0 15 37\r\n37 52 2\r\n39 0 15\r\n\r\nfertilizer-to-water map:\r\n49 53 8\r\n0 11 42\r\n42 0 7\r\n57 7 4\r\n\r\nwater-to-light map:\r\n88 18 7\r\n18 25 70\r\n\r\nlight-to-temperature map:\r\n45 77 23\r\n81 45 19\r\n68 64 13\r\n\r\ntemperature-to-humidity map:\r\n0 69 1\r\n1 0 69\r\n\r\nhumidity-to-location map:\r\n60 56 37\r\n56 93 4", "46")]
         [TestCase(6, 1, "Time:      7  15   30\r\nDistance:  9  40  200", "288")]
         [TestCase(6, 2, "Time:      7  15   30\r\nDistance:  9  40  200", "71503")]
+        [TestCase(7, 1, "32T3K 765\r\nT55J5 684\r\nKK677 28\r\nKTJJT 220\r\nQQQJA 483", "6440")]
+        [TestCase(7, 2, "32T3K 765\r\nT55J5 684\r\nKK677 28\r\nKTJJT 220\r\nQQQJA 483", "5905")]
         public void Tests(int day, int part, string input, string expectedResult)
         {
             ISolution solution = SolutionFactory.GetSolution(2023, day, part);
--- AdventOfCode/Year2023/Day07/Part02.cs
+++ AdventOfCode/Year2023/Day07/Part02.cs
@@ -1,12 +1,128 @@
 ﻿namespace AdventOfCode.Year2023.Day07
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using AdventOfCode;
 
     public class Part02 : ISolution
     {
+        private static readonly Dictionary<char, int> CardValues = new Dictionary<char, int>()
+        {
+            { 'A', 14 },
+            { 'K', 13 },
+            { 'Q', 12 },
+            { 'J', 1 },
+            { 'T', 10 },
+            { '9', 9 },
+            { '8', 8 },
+            { '7', 7 },
+            { '6', 6 },
+            { '5', 5 },
+            { '4', 4 },
+            { '3', 3 },
+            { '2', 2 },
+        };
+
         public string Solve(string[] input)
         {
-            return string.Empty;
+            IEnumerable<Hand> hands = input.Select(Hand.Create).Order();
+
+            int rank = 1;
+            int winnings = 0;
+            foreach (Hand hand in hands)
+            {
+                winnings += hand.Bid * rank;
+                ++rank;
+            }
+
+            return winnings.ToString();
+        }
+
+        public struct Hand : IComparable<Hand>
+        {
+            public static Hand Create(string input)
+            {
+                char[] cards = input[0..5].ToCharArray();
+                int bid = int.Parse(input[6..]);
+
+                IEnumerable<char> nonJokers = cards.Where(x => x != 'J');
+                int jokerCount = 5 - nonJokers.Count();
+
+                int strength = 0;
+
+                if (jokerCount == 5)
+                {
+                    strength = 7;
+                }
+                else
+                {
+                    (char Card, int Count)[] groupedCards = nonJokers.GroupBy(x => x).Select(x => (x.Key, x.Count())).OrderByDescending(x => x.Item2).ToArray();
+
+                    if (groupedCards.Length == 1)
+                    {
+                        strength = 7; // 5 of a kind
+                    }
+                    else if (groupedCards.Length == 2 && groupedCards[0].Count + jokerCount == 4)
+                    {
+                        strength = 6; // 4 of a kind
+                    }
+                    else if (groupedCards.Length == 2)
+                    {
+                        strength = 5; // Full house
+                    }
+                    else if (groupedCards.Length == 3 && groupedCards[0].Count + jokerCount == 3)
+                    {
+                        strength = 4; // 3 of a kind
+                    }
+                    else if (groupedCards.Length == 3)
+                    {
+                        strength = 3; // 2 pair
+                    }
+                    else if (groupedCards.Length == 4)
+                    {
+                        strength = 2; // 1 pair
+                    }
+                    else
+                    {
+                        strength = 1; // High card
+                    }
+                }
+
+                return new Hand
+                {
+                    Cards = cards,
+                    Type = strength,
+                    Bid = bid,
+                };
+            }
+
+            public char[] Cards { get; set; }
+
+            public int Bid { get; set; }
+
+            public int Type { get; set; }
+
+            public int CompareTo(Hand other)
+            {
+                if (this.Type != other.Type)
+                {
+                    return this.Type - other.Type;
+                }
+
+                // Same rank; fall back to card strength
+                for (int card = 0; card < this.Cards.Length; ++card)
+                {
+                    int thisStrength = CardValues[this.Cards[card]];
+                    int thatStrength = CardValues[other.Cards[card]];
+                    if (thisStrength != thatStrength)
+                    {
+                        return thisStrength - thatStrength;
+                    }
+                }
+
+                return 0;
+            }
         }
     }
 }
