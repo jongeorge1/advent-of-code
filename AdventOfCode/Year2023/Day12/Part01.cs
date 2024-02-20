938b650567ecbe708a9622f850e5c59cef20de45
Jonathan George <jon@jonathangeorge.co.uk>
2024-02-20 16:58:40 +0100

    Empty days

---
+++ AdventOfCode/Year2023/Day12/Part01.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Year2023.Day12
+{
+    using AdventOfCode;
+
+    public class Part01 : ISolution
+    {
+        public string Solve(string[] input)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ AdventOfCode/Year2023/Day14/Part02.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Year2023.Day14
+{
+    using AdventOfCode;
+
+    public class Part02 : ISolution
+    {
+        public string Solve(string[] input)
+        {
+            return string.Empty;
+        }
+    }
+}
