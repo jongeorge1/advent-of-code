733a9197fc2a8901f7843dd76d5f9e657b87e716
Jonathan George <jon@jonathangeorge.co.uk>
2024-02-20 18:25:00 +0100

    Day 11

--- AdventOfCode/Year2023/AoCTestCases.cs
+++ AdventOfCode/Year2023/AoCTestCases.cs
@@ -33,6 +33,7 @@ namespace AdventOfCode.Year2023
         [TestCase(10, 2, "...........\r\n.S-------7.\r\n.|F-----7|.\r\n.||.....||.\r\n.||.....||.\r\n.|L-7.F-J|.\r\n.|..|.|..|.\r\n.L--J.L--J.\r\n...........", "4")]
         [TestCase(10, 2, ".F----7F7F7F7F-7....\r\n.|F--7||||||||FJ....\r\n.||.FJ||||||||L7....\r\nFJL7L7LJLJ||LJ.L-7..\r\nL--J.L7...LJS7F-7L7.\r\n....F-J..F7FJ|L7L7L7\r\n....L7.F7||L7|.L7L7|\r\n.....|FJLJ|FJ|F7|.LJ\r\n....FJL-7.||.||||...\r\n....L---J.LJ.LJLJ...", "8")]
         [TestCase(10, 2, "FF7FSF7F7F7F7F7F---7\r\nL|LJ||||||||||||F--J\r\nFL-7LJLJ||||||LJL-77\r\nF--JF--7||LJLJ7F7FJ-\r\nL---JF-JLJ.||-FJLJJ7\r\n|F|F-JF---7F7-L7L|7|\r\n|FFJF7L7F-JF7|JL---7\r\n7-L-JL7||F7|L7F-7F7|\r\nL.L7LFJ|||||FJL7||LJ\r\nL7JLJL-JLJLJL--JLJ.L", "10")]
+        [TestCase(11, 1, "...#......\r\n.......#..\r\n#.........\r\n..........\r\n......#...\r\n.#........\r\n.........#\r\n..........\r\n.......#..\r\n#...#.....", "374")]
         public void Tests(int day, int part, string input, string expectedResult)
         {
             ISolution solution = SolutionFactory.GetSolution(2023, day, part);
--- AdventOfCode/Year2023/Day11/Part02.cs
+++ AdventOfCode/Year2023/Day11/Part02.cs
@@ -1,12 +1,84 @@
 ﻿namespace AdventOfCode.Year2023.Day11
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using AdventOfCode;
+    using AdventOfCode.Helpers;
+    using NUnit.Framework;
 
     public class Part02 : ISolution
     {
         public string Solve(string[] input)
         {
-            return string.Empty;
+            List<Galaxy> galaxies = new ();
+
+            List<int> emptyRows = new ();
+            List<int> emptyColumns = new ();
+
+            // Make a list of rows and columns that have no galaxies in them
+            for (int y = 0; y < input.Length; y++)
+            {
+                if (input[y].IndexOf('#') == -1)
+                {
+                    emptyRows.Add(y);
+                }
+            }
+
+            for (int x = 0; x < input[0].Length; x++)
+            {
+                if (!input.Any(row => row[x] == '#'))
+                {
+                    emptyColumns.Add(x);
+                }
+            }
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                if (emptyRows.Contains(y))
+                {
+                    continue;
+                }
+
+                long yExpansion = emptyRows.Count(i => i < y) * 999999;
+
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    if (input[y][x] == '#')
+                    {
+                        long xExpansion = emptyColumns.Count(i => i < x) * 999999;
+                        galaxies.Add(new Galaxy(x + xExpansion, y + yExpansion));
+                    }
+                }
+            }
+
+            long cumulativeDistance = 0;
+
+            // Now calculate the distances. Shortest distance between each galaxy is the Manhattan distance.
+            for (int leftIndex = 0; leftIndex < galaxies.Count; leftIndex++)
+            {
+                for (int rightIndex = leftIndex + 1; rightIndex < galaxies.Count; rightIndex++)
+                {
+                    Galaxy left = galaxies[leftIndex];
+                    Galaxy right = galaxies[rightIndex];
+                    long distance = Distance.Manhattan((left.X, left.Y), (right.X, right.Y));
+                    cumulativeDistance += distance;
+                }
+            }
+
+            return cumulativeDistance.ToString();
+        }
+
+        private struct Galaxy
+        {
+            public Galaxy(long x, long y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public long X { get; }
+
+            public long Y { get; }
         }
     }
 }
