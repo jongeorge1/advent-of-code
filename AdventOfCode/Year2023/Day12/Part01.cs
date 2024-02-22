ca22427cd230944a06ead7e5d6dfec36444075b4
Jonathan George <jon@jonathangeorge.co.uk>
2024-02-22 13:28:13 +0100

    Make part 2 recursive to make it easier to hold previously seen states.

--- AdventOfCode/Year2023/Day12/Part01.cs
+++ AdventOfCode/Year2023/Day12/Part01.cs
@@ -27,152 +27,88 @@
             int spacePosition = item.IndexOf(' ');
             string springs = item[..spacePosition];
             int[] expectedGroups = item[(spacePosition + 1) ..].Split(',').Select(int.Parse).ToArray();
-            int validArrangements = 0;
 
-            int springsCount = springs.Length;
+            Dictionary<int, int> seenStates = new();
+            return GetPossibleArrangementCountFromPosition(springs, 0, expectedGroups, seenStates);
+        }
 
-            Queue<EvaluationState> evaluations = new ();
-            evaluations.Enqueue(new EvaluationState(0, 0, 0));
+        private static int GetPossibleArrangementCountFromPosition(string map, int position, int[] remainingGroupsToMatch, Dictionary<int, int> seenStates)
+        {
+            int possibleCombinations = 0;
 
-            while (evaluations.TryDequeue(out EvaluationState? current))
+            // Have we been in this position before? We need a key that captures both position and remaining group count.
+            int key = (position << 8) + remainingGroupsToMatch.Length;
+            if (seenStates.TryGetValue(key, out int storedCombinations))
             {
-                if (current.IsComplete(springsCount))
-                {
-                    if (current.ExpectedGroupsFound(expectedGroups))
-                    {
-                        ++validArrangements;
-                    }
+                return storedCombinations;
+            }
 
-                    continue;
+            // Are there remaining groups to match?
+            if (remainingGroupsToMatch.Length == 0)
+            {
+                // This only works if there are no more broken springs
+                if (position >= map.Length || map.IndexOf('#', position) == -1)
+                {
+                    seenStates.Add(key, 1);
+                    return 1;
                 }
 
-                if (springs[current.EvaluatedSprings] == '?')
-                {
-                    if (TryCreateNewEvaluationState(current, '.', springsCount, expectedGroups, out EvaluationState? newWorkingState))
-                    {
-                        evaluations.Enqueue(newWorkingState);
-                    }
+                // If we're here, it doesn't work
+                seenStates.Add(key, 0);
+                return 0;
+            }
 
-                    if (TryCreateNewEvaluationState(current, '#', springsCount, expectedGroups, out EvaluationState? newBrokenState))
-                    {
-                        evaluations.Enqueue(newBrokenState);
-                    }
+            // Have we reached the end?
+            if (position >= map.Length)
+            {
+                // We must still have groups to match, otherwise we'd have gone through the conditional block above. So
+                // this doesn't work.
+                return 0;
+            }
+
+            // Now see what type of space we're on. If it's a . (or if it could be a .), we'll move on a step and stash the results.
+            if (map[position] == '.' || map[position] == '?')
+            {
+                possibleCombinations += GetPossibleArrangementCountFromPosition(map, position + 1, remainingGroupsToMatch, seenStates);
+            }
+
+            // If it's a '#' or it could be a '#' then we're starting a new group. Have a look at the expected next
+            // group size and work out if that's possible.
+            bool canMakeNextGroup = true;
+            if (map[position] == '#' || map[position] == '?')
+            {
+                int expectedNextGroupSize = remainingGroupsToMatch[0];
+                if (position + expectedNextGroupSize > map.Length)
+                {
+                    canMakeNextGroup = false;
                 }
                 else
                 {
-                    if (TryCreateNewEvaluationState(
-                        current,
-                        springs[current.EvaluatedSprings],
-                        springsCount,
-                        expectedGroups,
-                        out EvaluationState? newState))
+                    for (int i = position + 1; i < position + expectedNextGroupSize; ++i)
                     {
-                        evaluations.Enqueue(newState);
+                        if (map[i] == '.')
+                        {
+                            canMakeNextGroup = false;
+                            break;
+                        }
                     }
                 }
-            }
 
-            return validArrangements;
-        }
-
-        private static bool TryCreateNewEvaluationState(
-            EvaluationState current,
-            char newSpringState,
-            int expectedSpringsCount,
-            int[] expectedGroups,
-            [NotNullWhen(true)] out EvaluationState? newEvaluationState)
-        {
-            newEvaluationState = null;
-
-            if (newSpringState == '.')
-            {
-                if (current.CurrentGroupSize > 0)
+                // We can successfully make the next group. We now just need to check that the next character is not a '#'
+                if (canMakeNextGroup && (position + expectedNextGroupSize < map.Length) && map[position + expectedNextGroupSize] == '#')
                 {
-                    // We've just finished off a group. Compare it against the expected next group size
-                    // and return a new state if it's correct.
-                    if (current.CurrentGroupSize == expectedGroups[current.MatchedGroups])
-                    {
-                        // All good. Return a new state
-                        newEvaluationState = new EvaluationState(
-                            current.EvaluatedSprings + 1,
-                            current.MatchedGroups + 1,
-                            0);
-
-                        return newEvaluationState.CanComplete(expectedSpringsCount, expectedGroups);
-                    }
-
-                    return false;
+                    canMakeNextGroup = false;
                 }
 
-                // We weren't in a group, and we still aren't
-                newEvaluationState = new EvaluationState(
-                    current.EvaluatedSprings + 1,
-                    current.MatchedGroups,
-                    0);
-
-                return newEvaluationState.CanComplete(expectedSpringsCount, expectedGroups);
-            }
-
-            // We're in a group. If we weren't in one already, we should check to see that we're expecting
-            // another group
-            if (current.CurrentGroupSize == 0 && current.MatchedGroups == expectedGroups.Length)
-            {
-                return false;
-            }
-
-            // If we are in one, we should check to see that we haven't exceeded the next expected group size.
-            // (Use > because we are about to add one to the current group size.
-            if (current.CurrentGroupSize >= expectedGroups[current.MatchedGroups])
-            {
-                return false;
-            }
-
-            // We're in a group. We don't need to do anything special here, because we'll evaluate
-            // whether things are correct once we've exited the group.
-            newEvaluationState = new EvaluationState(
-                current.EvaluatedSprings + 1,
-                current.MatchedGroups,
-                current.CurrentGroupSize + 1);
-
-            return newEvaluationState.CanComplete(expectedSpringsCount, expectedGroups);
-        }
-
-        private record EvaluationState(int EvaluatedSprings, int MatchedGroups, int CurrentGroupSize)
-        {
-            public bool IsComplete(int expectedSpringsCount)
-            {
-                return this.EvaluatedSprings == expectedSpringsCount;
-            }
-
-            public bool CanComplete(int expectedSpringsCount, int[] expectedGroups)
-            {
-                int remainingSprings = expectedSpringsCount - this.EvaluatedSprings;
-                int[] remainingGroups = expectedGroups[this.MatchedGroups..];
-                int minimumRequiredRemainingSprings = remainingGroups.Sum() + remainingGroups.Count() - 1 - this.CurrentGroupSize;
-                return remainingSprings >= minimumRequiredRemainingSprings;
-            }
-
-            public bool ExpectedGroupsFound(int[] expectedGroups)
-            {
-                int matchedGroups = this.MatchedGroups;
-
-                if (this.CurrentGroupSize > 0)
+                if (canMakeNextGroup)
                 {
-                    // We were in a group when we hit the end. If there's exactly one group left to match,
-                    // and it's the right size, then we're good. Otherwise not.
-                    if (expectedGroups[this.MatchedGroups] == this.CurrentGroupSize)
-                    {
-                        matchedGroups++;
-                    }
-                    else
-                    {
-                        // The group size didn't match, so this can't be valid.
-                        return false;
-                    }
+                    // See what comes next
+                    possibleCombinations += GetPossibleArrangementCountFromPosition(map, position + expectedNextGroupSize + 1, remainingGroupsToMatch[1..], seenStates);
                 }
-
-                return matchedGroups == expectedGroups.Length;
             }
+
+            seenStates.Add(key, possibleCombinations);
+            return possibleCombinations;
         }
     }
 }
--- AdventOfCode/Year2023/Day12/Part02.cs
+++ AdventOfCode/Year2023/Day12/Part02.cs
@@ -12,18 +12,17 @@
     {
         public string Solve(string[] input)
         {
-            int runningTotal = 0;
+            long runningTotal = 0;
 
             foreach (string item in input)
             {
                 runningTotal += GetPossibleArrangementCount(item);
-                Console.WriteLine($"{runningTotal} arrangements found");
             }
 
             return runningTotal.ToString();
         }
 
-        private static int GetPossibleArrangementCount(string item)
+        private static long GetPossibleArrangementCount(string item)
         {
             // Split into the spring map and checksums
             int spacePosition = item.IndexOf(' ');
@@ -41,162 +40,88 @@
             expandedGroups.AddRange(expectedGroups);
 
             expectedGroups = expandedGroups.ToArray();
-            int validArrangements = 0;
 
-            int springsCount = springs.Length;
+            Dictionary<int, long> seenStates = new();
+            return GetPossibleArrangementCountFromPosition(springs, 0, expectedGroups, seenStates);
+        }
 
-            Stack<EvaluationState> evaluations = new();
-            evaluations.Push(new EvaluationState(0, 0, 0));
+        private static long GetPossibleArrangementCountFromPosition(string map, int position, int[] remainingGroupsToMatch, Dictionary<int, long> seenStates)
+        {
+            long possibleCombinations = 0;
 
-            Dictionary<int, int> seenStates = new();
-
-            while (evaluations.TryPop(out EvaluationState? current))
+            // Have we been in this position before? We need a key that captures both position and remaining group count.
+            int key = (position << 8) + remainingGroupsToMatch.Length;
+            if (seenStates.TryGetValue(key, out long storedCombinations))
             {
-                if (current.IsComplete(springsCount))
-                {
-                    if (current.ExpectedGroupsFound(expectedGroups))
-                    {
-                        ++validArrangements;
-                    }
+                return storedCombinations;
+            }
 
-                    continue;
+            // Are there remaining groups to match?
+            if (remainingGroupsToMatch.Length == 0)
+            {
+                // This only works if there are no more broken springs
+                if (position >= map.Length || map.IndexOf('#', position) == -1)
+                {
+                    seenStates.Add(key, 1);
+                    return 1;
                 }
 
-                if (springs[current.EvaluatedSprings] == '?')
-                {
-                    // Could be either. We can be a bit intelligent here; if we're in a group and the group is
-                    // already at the expected length, we shouldn't add another broken spring. Similarly, if we've
-                    // already evaluated all groups, we shouldn't add another broken spring.
-                    if (current.MatchedGroups < expectedGroups.Length && current.CurrentGroupSize < expectedGroups[current.MatchedGroups])
-                    {
-                        if (TryCreateNewEvaluationState(current, '#', springsCount, expectedGroups, out EvaluationState? newBrokenState))
-                        {
-                            evaluations.Push(newBrokenState);
-                        }
-                    }
+                // If we're here, it doesn't work
+                seenStates.Add(key, 0);
+                return 0;
+            }
 
-                    if (TryCreateNewEvaluationState(current, '.', springsCount, expectedGroups, out EvaluationState? newWorkingState))
-                    {
-                        evaluations.Push(newWorkingState);
-                    }
+            // Have we reached the end?
+            if (position >= map.Length)
+            {
+                // We must still have groups to match, otherwise we'd have gone through the conditional block above. So
+                // this doesn't work.
+                return 0;
+            }
+
+            // Now see what type of space we're on. If it's a . (or if it could be a .), we'll move on a step and stash the results.
+            if (map[position] == '.' || map[position] == '?')
+            {
+                possibleCombinations += GetPossibleArrangementCountFromPosition(map, position + 1, remainingGroupsToMatch, seenStates);
+            }
+
+            // If it's a '#' or it could be a '#' then we're starting a new group. Have a look at the expected next
+            // group size and work out if that's possible.
+            bool canMakeNextGroup = true;
+            if (map[position] == '#' || map[position] == '?')
+            {
+                int expectedNextGroupSize = remainingGroupsToMatch[0];
+                if (position + expectedNextGroupSize > map.Length)
+                {
+                    canMakeNextGroup = false;
                 }
                 else
                 {
-                    if (TryCreateNewEvaluationState(
-                        current,
-                        springs[current.EvaluatedSprings],
-                        springsCount,
-                        expectedGroups,
-                        out EvaluationState? newState))
+                    for (int i = position + 1; i < position + expectedNextGroupSize; ++i)
                     {
-                        evaluations.Push(newState);
+                        if (map[i] == '.')
+                        {
+                            canMakeNextGroup = false;
+                            break;
+                        }
                     }
                 }
 
-                ////Console.WriteLine($"    Evaluating {evaluations.Count} possibilities");
-            }
-
-            return validArrangements;
-        }
-
-        private static bool TryCreateNewEvaluationState(
-            EvaluationState current,
-            char newSpringState,
-            int expectedSpringsCount,
-            int[] expectedGroups,
-            [NotNullWhen(true)] out EvaluationState? newEvaluationState)
-        {
-            newEvaluationState = null;
-
-            if (newSpringState == '.')
-            {
-                if (current.CurrentGroupSize > 0)
+                // We can successfully make the next group. We now just need to check that the next character is not a '#'
+                if (canMakeNextGroup && (position + expectedNextGroupSize < map.Length) && map[position + expectedNextGroupSize] == '#')
                 {
-                    // We've just finished off a group. Compare it against the expected next group size
-                    // and return a new state if it's correct.
-                    if (current.CurrentGroupSize == expectedGroups[current.MatchedGroups])
-                    {
-                        // All good. Return a new state
-                        newEvaluationState = new EvaluationState(
-                            current.EvaluatedSprings + 1,
-                            current.MatchedGroups + 1,
-                            0);
-
-                        return newEvaluationState.CanComplete(expectedSpringsCount, expectedGroups);
-                    }
-
-                    return false;
+                    canMakeNextGroup = false;
                 }
 
-                // We weren't in a group, and we still aren't
-                newEvaluationState = new EvaluationState(
-                    current.EvaluatedSprings + 1,
-                    current.MatchedGroups,
-                    0);
-
-                return newEvaluationState.CanComplete(expectedSpringsCount, expectedGroups);
-            }
-
-            // We're in a group. If we weren't in one already, we should check to see that we're expecting
-            // another group
-            if (current.CurrentGroupSize == 0 && current.MatchedGroups == expectedGroups.Length)
-            {
-                return false;
-            }
-
-            // If we are in one, we should check to see that we haven't exceeded the next expected group size.
-            // (Use > because we are about to add one to the current group size.
-            if (current.CurrentGroupSize >= expectedGroups[current.MatchedGroups])
-            {
-                return false;
-            }
-
-            // We're in a group. We don't need to do anything special here, because we'll evaluate
-            // whether things are correct once we've exited the group.
-            newEvaluationState = new EvaluationState(
-                current.EvaluatedSprings + 1,
-                current.MatchedGroups,
-                current.CurrentGroupSize + 1);
-
-            return newEvaluationState.CanComplete(expectedSpringsCount, expectedGroups);
-        }
-
-        private record EvaluationState(int EvaluatedSprings, int MatchedGroups, int CurrentGroupSize)
-        {
-            public bool IsComplete(int expectedSpringsCount)
-            {
-                return this.EvaluatedSprings == expectedSpringsCount;
-            }
-
-            public bool CanComplete(int expectedSpringsCount, int[] expectedGroups)
-            {
-                int remainingSprings = expectedSpringsCount - this.EvaluatedSprings;
-                int[] remainingGroups = expectedGroups[this.MatchedGroups..];
-                int minimumRequiredRemainingSprings = remainingGroups.Sum() + remainingGroups.Count() - 1 - this.CurrentGroupSize;
-                return remainingSprings >= minimumRequiredRemainingSprings;
-            }
-
-            public bool ExpectedGroupsFound(int[] expectedGroups)
-            {
-                int matchedGroups = this.MatchedGroups;
-
-                if (this.CurrentGroupSize > 0)
+                if (canMakeNextGroup)
                 {
-                    // We were in a group when we hit the end. If there's exactly one group left to match,
-                    // and it's the right size, then we're good. Otherwise not.
-                    if (expectedGroups[this.MatchedGroups] == this.CurrentGroupSize)
-                    {
-                        matchedGroups++;
-                    }
-                    else
-                    {
-                        // The group size didn't match, so this can't be valid.
-                        return false;
-                    }
+                    // See what comes next
+                    possibleCombinations += GetPossibleArrangementCountFromPosition(map, position + expectedNextGroupSize + 1, remainingGroupsToMatch[1..], seenStates);
                 }
-
-                return matchedGroups == expectedGroups.Length;
             }
+
+            seenStates.Add(key, possibleCombinations);
+            return possibleCombinations;
         }
     }
 }
