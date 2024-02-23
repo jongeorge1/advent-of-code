0f8d6976ec4fe4f85dfdf4968118415740a984eb
Jonathan George <jon@jonathangeorge.co.uk>
2024-02-23 18:33:58 +0100

    14.1

--- AdventOfCode/Year2023/Day14/Part01.cs
+++ AdventOfCode/Year2023/Day14/Part01.cs
@@ -6,7 +6,56 @@
     {
         public string Solve(string[] input)
         {
-            return string.Empty;
+            int runningTotal = 0;
+
+            // Column by column...
+            for (int column = 0; column < input[0].Length; column++)
+            {
+                // Row by row
+                int firstEmptyRow = 0;
+                int currentRow = 0;
+
+                while (currentRow < input.Length)
+                {
+                    // What's in the current row?
+                    switch (input[currentRow][column])
+                    {
+                        case '.':
+                            // Nothing of interest. Crack on.
+                            break;
+
+                        case '#':
+                            // A cube-shaped rock. This means we need to update our "first empty row" as rocks
+                            // cannot come past this point.
+                            firstEmptyRow = currentRow + 1;
+
+                            break;
+
+                        case 'O':
+                            // This rock might be able to roll back...
+                            if (firstEmptyRow < currentRow)
+                            {
+                                // It can roll. It will go as far as the first empty row and stop, so we know
+                                // how much load it will contribute...
+                                runningTotal += input.Length - firstEmptyRow;
+                                ++firstEmptyRow;
+                            }
+                            else
+                            {
+                                // It can't roll, so it must stay where it is. Just score it as-is.
+                                runningTotal += input.Length - currentRow;
+                                firstEmptyRow = currentRow + 1;
+                            }
+
+                            break;
+                    }
+
+                    ++currentRow;
+                }
+
+            }
+
+            return runningTotal.ToString();
         }
     }
 }
