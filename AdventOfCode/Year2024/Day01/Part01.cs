10d563447a599dae6e3b94d1e81e615bc3a8aabd
Jonathan George <github@jonathangeorge.co.uk>
2024-12-02 10:17:52 +0000

    Prep

--- AdventOfCode/Year2024/Day01/Part01.cs
+++ AdventOfCode/Year2024/Day01/Part01.cs
@@ -1,33 +1,32 @@
-﻿namespace AdventOfCode.Year2024.Day01
+﻿namespace AdventOfCode.Year2024.Day01;
+
+using System;
+using AdventOfCode;
+
+public class Part01 : ISolution
 {
-    using System;
-    using AdventOfCode;
-
-    public class Part01 : ISolution
+    public string Solve(string[] input)
     {
-        public string Solve(string[] input)
+        Span<int> left = stackalloc int[input.Length];
+        Span<int> right = stackalloc int[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
         {
-            Span<int> left = stackalloc int[input.Length];
-            Span<int> right = stackalloc int[input.Length];
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                int length = input[i].Length / 2;
-                left[i] = int.Parse(input[i].AsSpan(0, length));
-                right[i] = int.Parse(input[i].AsSpan(length));
-            }
-
-            MemoryExtensions.Sort(left);
-            MemoryExtensions.Sort(right);
-
-            int sum = 0;
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                sum += Math.Abs(left[i] - right[i]);
-            }
-
-            return sum.ToString();
+            int length = input[i].Length / 2;
+            left[i] = int.Parse(input[i].AsSpan(0, length));
+            right[i] = int.Parse(input[i].AsSpan(length));
         }
+
+        MemoryExtensions.Sort(left);
+        MemoryExtensions.Sort(right);
+
+        int sum = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            sum += Math.Abs(left[i] - right[i]);
+        }
+
+        return sum.ToString();
     }
 }
--- AdventOfCode/Year2024/Day01/Part02.cs
+++ AdventOfCode/Year2024/Day01/Part02.cs
@@ -1,41 +1,40 @@
-﻿namespace AdventOfCode.Year2024.Day01
+﻿namespace AdventOfCode.Year2024.Day01;
+
+using System;
+using AdventOfCode;
+
+public class Part02 : ISolution
 {
-    using System;
-    using AdventOfCode;
-
-    public class Part02 : ISolution
+    public string Solve(string[] input)
     {
-        public string Solve(string[] input)
+        Span<int> left = stackalloc int[input.Length];
+        Span<int> right = stackalloc int[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
         {
-            Span<int> left = stackalloc int[input.Length];
-            Span<int> right = stackalloc int[input.Length];
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                int length = input[i].Length / 2;
-                left[i] = int.Parse(input[i].AsSpan(0, length));
-                right[i] = int.Parse(input[i].AsSpan(length));
-            }
-
-            int totalSimilarity = 0;
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                int target = left[i];
-
-                int occurrences = 0;
-                for (int j = 0; j < input.Length; j++)
-                {
-                    if (right[j] == target)
-                    {
-                        occurrences++;
-                    }
-                }
-
-                totalSimilarity += target * occurrences;
-            }
-
-            return totalSimilarity.ToString();
+            int length = input[i].Length / 2;
+            left[i] = int.Parse(input[i].AsSpan(0, length));
+            right[i] = int.Parse(input[i].AsSpan(length));
         }
+
+        int totalSimilarity = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            int target = left[i];
+
+            int occurrences = 0;
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (right[j] == target)
+                {
+                    occurrences++;
+                }
+            }
+
+            totalSimilarity += target * occurrences;
+        }
+
+        return totalSimilarity.ToString();
     }
 }
