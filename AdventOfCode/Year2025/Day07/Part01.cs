bb71dfbf82a78b2ef44c16932235ed10b6f10055
Jonathan George <github@jonathangeorge.co.uk>
2025-12-08 00:00:30 +0000

    Day 7

--- AdventOfCode/Year2025/Day07/Part01.cs
+++ AdventOfCode/Year2025/Day07/Part01.cs
@@ -1,11 +1,66 @@
 namespace AdventOfCode.Year2025.Day07;
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using AdventOfCode;
+using AdventOfCode.Helpers;
 
 public class Part01 : ISolution
 {
     public string Solve(string[] input)
     {
-        return string.Empty;
+        // Parse the input, finding:
+        // 1. The start location (always in row 0)
+        // 2. The number of rows (important so we know when to stop tracing a beam
+        // 3. The number of columns (important so we know when to stop tracing a beam)
+        // 4. The locations of all the splitters
+        (int X, int Y) startLocation = (input[0].IndexOf('S'), 0);
+        int maxRow = input.Length - 1;
+        int maxCol = input[0].Length - 1;
+
+        (int X, int Y)[] splitters = [.. input
+            .SelectMany((row, rowIndex) => row.Select((col, colIndex) => (col, (colIndex, rowIndex))))
+            .Where(t => t.col == '^')
+            .Select(x => x.Item2)];
+
+        List<(int X, int Y)> processedLocations = [];
+        Stack<(int X, int Y)> locationsToProcess = [];
+        locationsToProcess.Push(startLocation);
+        int splitCount = 0;
+        Direction2D beamDirection = Direction2D.South;
+
+        while (locationsToProcess.Count > 0)
+        {
+            (int X, int Y) locationToProcess = locationsToProcess.Pop();
+            if (processedLocations.Contains(locationToProcess))
+            {
+                continue;
+            }
+
+            processedLocations.Add(locationToProcess);
+            (int X, int Y) nextLocation = beamDirection.GetNextLocation(locationToProcess);
+
+            // Is it a splitter location?
+            if (splitters.Contains(nextLocation))
+            {
+                splitCount++;
+
+                // Add both new directions to the stack
+                locationsToProcess.Push(Direction2D.East.GetNextLocation(nextLocation));
+                locationsToProcess.Push(Direction2D.West.GetNextLocation(nextLocation));
+            }
+            else
+            {
+                // Is it still in bounds?
+                if (nextLocation.X >= 0 && nextLocation.X <= maxCol &&
+                    nextLocation.Y >= 0 && nextLocation.Y <= maxRow)
+                {
+                    locationsToProcess.Push(nextLocation);
+                }
+            }
+        }
+
+        return splitCount.ToString();
     }
 }
--- AdventOfCode/Year2025/Day07/Part02.cs
+++ AdventOfCode/Year2025/Day07/Part02.cs
@@ -1,11 +1,58 @@
 namespace AdventOfCode.Year2025.Day07;
 
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode;
+using AdventOfCode.Helpers;
 
 public class Part02 : ISolution
 {
     public string Solve(string[] input)
     {
-        return string.Empty;
+        (int X, int Y) startLocation = (input[0].IndexOf('S'), 0);
+        int maxRow = input.Length - 1;
+        int maxCol = input[0].Length - 1;
+
+        (int X, int Y)[] splitters = [.. input
+            .SelectMany((row, rowIndex) => row.Select((col, colIndex) => (col, (colIndex, rowIndex))))
+            .Where(t => t.col == '^')
+            .Select(x => x.Item2)];
+
+        Stack<(int X, int Y)> locationsToProcess = [];
+        locationsToProcess.Push(startLocation);
+        Dictionary<(int X, int Y), long> realityCountsFromLocations = [];
+        long realityCount = GetRealityCountFrom(startLocation, splitters, maxCol, maxRow, realityCountsFromLocations);
+        return realityCount.ToString();
+    }
+
+    private static long GetRealityCountFrom((int X, int Y) location, (int X, int Y)[] splitters, int maxX, int maxY, Dictionary<(int X, int Y), long> realityCountsFromLocations)
+    {
+        // If we've already calculated the reality count from this location, return it
+        if (realityCountsFromLocations.ContainsKey(location))
+        {
+            return realityCountsFromLocations[location];
+        }
+
+        // If location is out of bounds, return 1.
+        if (location.X < 0 || location.X > maxX || location.Y < 0 || location.Y > maxY)
+        {
+            return 1;
+        }
+
+        (int X, int Y) nextLocation = Direction2D.South.GetNextLocation(location);
+        if (splitters.Contains(nextLocation))
+        {
+            // Recursive case: calculate the reality count for both new directions
+            long eastCount = GetRealityCountFrom(Direction2D.East.GetNextLocation(nextLocation), splitters, maxX, maxY, realityCountsFromLocations);
+            long westCount = GetRealityCountFrom(Direction2D.West.GetNextLocation(nextLocation), splitters, maxX, maxY, realityCountsFromLocations);
+            long totalCount = eastCount + westCount;
+            realityCountsFromLocations[location] = totalCount;
+            return totalCount;
+        }
+
+        // Continue moving south
+        long count = GetRealityCountFrom(nextLocation, splitters, maxX, maxY, realityCountsFromLocations);
+        realityCountsFromLocations[location] = count;
+        return count;
     }
 }
